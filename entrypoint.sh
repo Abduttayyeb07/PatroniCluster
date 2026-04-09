6229f64b7863960a80b25c4c7934ead61ff9582f
Abduttayyeb07 <abduttayyeb05@gmail.com>
2026-04-09 18:13:52 +0500

    fix: CRLF line endings on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-# Copy the mounted SSH key and fix permissions for botuser
+# Copy the mounted SSH key and fix permissions
 if [ -f /tmp/ssh_key ]; then
   cp /tmp/ssh_key /app/.ssh/id_ed25519
   chmod 600 /app/.ssh/id_ed25519
+  echo SSH key copied successfully
+else
+  echo WARNING: No SSH key found at /tmp/ssh_key
 fi
 
-exec "$@"
+exec " $@\
